6e57115c9ade6f3b09bdd0fe136385101d211772
Rodrigo Lacerda <rodrigolacerda360@gmail.com>
2022-10-03 20:21:58 -0300

    testes

---
+++ script.js
@@ -0,0 +1,10 @@
+// Configurações do menu mobile
+
+function click_menu() {
+    if(links.style.display == 'block') {
+        links.style.display = 'none';
+    }else {
+        links.style.display = 'block';
+    }
+
+}
